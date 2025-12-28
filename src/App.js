bf4a1ceda2c1b2e79e2f5f9c3428da2044f75829
Alvaro Carvalho de Lima <alvarocarv2004@gmail.com>
2025-12-28 17:07:49 -0300

    subindo a estrutura do projeto e complementando com algumas páginas de login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá mundo</h1>
+      <a href="login.html">Página de Login</a>
+
     </div>
   );
 }
