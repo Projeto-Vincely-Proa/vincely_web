5bc1e509383ddc8d4a1a631a7f09698aef24f90a
Alvaro Carvalho de Lima <alvarocarv2004@gmail.com>
2026-01-17 20:54:31 -0300

    adicionando cards na seção de landing

--- src/components/begin/landing/Landing.jsx
+++ src/components/begin/landing/Landing.jsx
@@ -9,20 +9,22 @@ const Landing = () => {
                 <div className="landing-line"></div>
                 <h2>Você está em boas mãos</h2>
 
-                <p>
-                    Seja a ansiedade que acelera seus pensamentos
-                    ou a depressão que torna seus dias mais pesados,
-                    você não precisa enfrentar isso sozinho.
+                <div className="landing-cards">
+                    <p className='card'>
+                        Seja a ansiedade que acelera seus pensamentos
+                        ou a depressão que torna seus dias mais pesados,
+                        você não precisa enfrentar isso sozinho.
 
-                </p>
+                    </p>
 
-                <p>
-                    Este é um espaço criado para acolher suas dores, oferecer apoio verdadeiro e mostrar que existe um caminho de esperança e transformação.
-                </p>
+                    <p className='card'>
+                        Este é um espaço criado para acolher suas dores, oferecer apoio verdadeiro e mostrar que existe um caminho de esperança e transformação.
+                    </p>
 
-                <p>
-                    Aqui você encontra compreensão, recursos e pessoas dispostas a caminhar ao seu lado, porque cuidar da sua mente é também reencontrar sua força e redescobrir o sentido da vida.
-                </p>
+                    <p className='card'>
+                        Aqui você encontra compreensão, recursos e pessoas dispostas a caminhar ao seu lado, porque cuidar da sua mente é também reencontrar sua força e redescobrir o sentido da vida.
+                    </p>
+                </div>
 
                 <button>
                     <span>Quero ajuda</span> <IoIosArrowRoundForward className='arrow' />
